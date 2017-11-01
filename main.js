054587aae47aeff75ee5bc1b22c4586a0bf45486
yoeriwalstra <yoeriwalstra@gmail.com>
2017-11-01 13:00:00 +0100

    commit1

---
+++ main.js
@@ -0,0 +1,19 @@
+/*document.getElementById('username').value
+document.getElementById('password').value
+
+function checkPassword (password) {
+	if (password === '12345678') {
+		window.location.href = //redirect naar pagina
+	}
+}
+
+function hasNumber(username) {
+	if(/\d/.test(username) {
+		//do something... console log, or redirect to new page, or...?
+	}
+}
+
+document.getElementById('h1').value*/
+
+var container = document.getElementById('container');
+container.innerHTML = "Hello World!"
